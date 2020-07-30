68cd30db54419899d51c1f299f05b68dae0bd274
Stephan Garland <stephan.garland@logicmonitor.com>
2020-07-30 17:30:26 -0500

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
+
 # terraform-0.12-replacer
-Replaces reserved word[s] in Terraform files in preparation for 0.12
+
+### WARNING: I AM IN NO WAY RESPONSIBLE IF YOU RUN THIS WITHOUT VERSION CONTROL AND THEN BREAK YOUR INFRASTRUCTURE.
+
+#### Who this is for:
+Anyone who needs to upgrade a large amount of Terraform <= 0.11 into 0.12-compliant HCL, but doesn't want to manually find and rename variables.
+
+#### What it does do:
+1. Replaces [reserved word\[s\]](https://www.terraform.io/docs/configuration/variables.html#declaring-an-input-variable) in Terraform files in preparation for 0.12.
+2. Pulls variables out of any .tf file not named variables.tf, and places them in variables.tf
+
+#### What it does not do:
+1. Actually perform the 0.12 upgrade. Hashicorp's own 0.12upgrade tool does a wonderful job at that.
+2. Work in all edge cases. If you find one, please submit a PR.
+3. Pull variables out if they are not in a contiguous block.
+4. Alpha-sort the variables.tf file. It could be done with a clever regex.
+5. Use a regex to find variables. There is a lengthy comment block around line 100 detailing a wonderfully complex regex that does in fact work for many cases, but not all. As such, I opted to not use it, and instead use readlines() and look for strings starting with "variable." If you can improve the regex, again, please submit a PR.
+6. Pretty up the resultant .tf files. There may be newlines or lack thereof. 0.12upgrade may handle some of those; I know it splits out one-line variables with definitions into multi-line.
+
+#### Requirements:
+1. Version - Python 3.x. Tested with 3.7.7, but it should probably work with anything 3.5+, as I'm using type hints. If you want to strip those out, it will probably work with 3.x, maybe 2.7. I have no interest in backporting it to 2.7 if it doesn't work.
+2. OS - It _should_ work on *nix and Windows. UNC paths on Windows would probably cause issues with os.path, but if you're accessing local files it should be fine. Developed on a Mac.
---
+++ tf_12_count_fixer.py
@@ -0,0 +1,144 @@
+#!/usr/bin/env python
+"""
+Author:         Stephan Garland, stephan.marc.garland@gmail.com
+Purpose:        Prepares files for terraform 0.12upgrade
+Description:    Converts `count` in Terraform modules to `num`
+                Shifts any variables into their own file
+                Replaces all instances of `var.count` with `var.num`
+
+License:
+This Source Code Form is subject to the terms of the Mozilla Public License,
+v. 2.0. If a copy of the MPL was not distributed with this file, 
+You can obtain one at https://mozilla.org/MPL/2.0/.
+"""
+import glob
+import os
+import re
+import sys
+import typing
+
+from pathlib import Path
+
+COUNT_REPLACEMENT_WORD = "num"
+
+# Count regex explanation
+# Group 1: Lines beginning with "module", followed by 1+ of any character except
+#          a linebreak, followed by 1+ linebreak
+# Group 2: 1+ of any character
+# Group 3: 1+ of a whitespace character with a positive lookahead of "count"
+# Group 4: The word "count"
+# Example:
+#
+# module "security-groups" {                                                                                <-- Group 1
+#     source = "./path"                                                                                     <-- Group 2
+#     literally_just_gibberish                                                                              <-- Group 2
+#     count  = "${var.count}"                                                                        <-- Groups 3 and 4
+# }
+
+def count_regex_func(f: typing.TextIO) -> None:
+    count_regex = r"(^module.+\n+)([\S\s].+)?(\s+(?=count))(count)"
+    count_regex_sub = r"\1\2\3" + COUNT_REPLACEMENT_WORD
+    count_var_regex = r"(var\.count)"
+    count_var_regex_sub = "var." + COUNT_REPLACEMENT_WORD
+    whole_file = f.read()
+    repl_count = re.sub(count_regex, count_regex_sub, whole_file, 0, re.MULTILINE)
+    repl_count_var = re.sub(count_var_regex, count_var_regex_sub, repl_count, 0, re.MULTILINE)
+    f.seek(0)
+    f.write(repl_count_var)
+    f.truncate()
+
+def strip_vars(f: typing.TextIO) -> list:
+    tmp_lst = []
+    i = 0
+    f.seek(0)
+    for line in f.readlines():
+        tmp_lst.append(line)
+        if line.startswith(tuple(ignore)):
+            # This assumes your .tf files have a contiguous block of variables
+            tmp_lst.pop()
+            break
+    for line in "".join(tmp_lst).splitlines():
+        if line.startswith("variable"):
+            break
+        while not line.startswith("variable"):
+            i += 1
+            break
+    del tmp_lst[:i]
+    # Remove trailing newline if it got pulled in
+    if "".join(tmp_lst[-1:]).isspace():
+        del tmp_lst[-1:]
+    # And add a newline to the head for any existing entries
+    if not "".join(tmp_lst[0]).isspace():
+        tmp_lst.insert(0, "\n")
+    return tmp_lst
+
+def write_var_file(f: typing.TextIO, var_lst: list) -> None:
+    tf_file = f.read()
+    with open(os.path.dirname(os.path.realpath(f.name)) + "/variables.tf", "a+") as var_file:
+        # Write the variables to their own file, and replace "count" with the specified name
+        var_file.write("".join(var_lst).replace("count", COUNT_REPLACEMENT_WORD))
+
+def remove_vars_from_main(f: typing.TextIO, var_lst: list) -> None:
+    # With variables written to a new file, remove them from the main file
+    with open(f.name) as tmp_file:
+        tf_file = tmp_file.read()
+        if "".join(var_lst) in tf_file:
+            tf_file = tf_file.replace("".join(tf_vars), "")
+            f.seek(0)
+            f.write(tf_file)
+            f.truncate()
+        else:
+            print("ERROR: Unable to move variables out of main file, please do so manually")
+
+# NOTE: This is not implemented, as it - as regexes are wont to do - fails on edge cases
+# Variable regex explanation
+# Group 1: 0+ of any character, with a negative lookahead of the word "variable" at a line's start
+#          This is used to discard anything prior to a variable block
+# Group 2: The word "variable" at a line's start, followed by 1+ of any character
+# Group 3: Either the string "{}" or "{" followed by 1+ a linebreak or whitespace character, followed by
+#          1+ of any character except linebreaks, followed by a linebreak or whitespace, followed by
+#          "}" at a line's end
+# Group 4: 1+ of any character
+
+# Example:
+# provider "aws" {                                                                                          <-- Group 1
+#   alias = "accepter"                                                                                      <-- Group 1
+# }                                                                                                         <-- Group 1
+#                                                                                                           <-- Group 1
+# variable "name" {}                                                                                        <-- Group 2
+# variable "count" {}                                                                                       <-- Group 2
+# variable "long_string" {                                                                           <-- Groups 2 and 3
+#     default = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod                    <-- Group 3
+#                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,               <-- Group 3
+#                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat..."      <-- Group 3
+# }                                                                                                         <-- Group 3                                                                                 
+# data "aws_route_tables" "requester" {                                                                     <-- Group 4
+#    provider = "aws.requester"                                                                             <-- Group 4
+#    vpc_id = "${var.requester_vpc_id}"                                                                     <-- Group 4
+# }                                                                                                         <-- Group 4
+
+#var_regex = r"((?!^variable)[\s\S])*(^variable[\s\S]+)({}$|{[\n\s].+[\n\s]}$)([\s\S]*)"
+#var_regex_sub = r"\2\3"
+
+# You can glob a Path object
+cwd = Path(os.getcwd())
+
+ignore = ["data", "locals", "module", "output", "provider", "resource"]
+tf_files = [x for x in cwd.glob("**/*.tf") if not x.name.startswith("variables")]
+for tf_file in tf_files:
+    # Regex modules and write the file
+    try:
+        with open(tf_file, "r+") as f:
+            count_regex_func(f)
+            tf_vars = strip_vars(f)
+            write_var_file(f, tf_vars)
+            remove_vars_from_main(f, tf_vars)
+
+            # Here is where I would be using var_regex_sub if I didn't suck at regexes
+            #whole_file = f.read()
+            #f.seek(0)
+            #repl_vars = re.sub(var_regex, var_regex_sub, whole_file, 0, re.MULTILINE)
+
+    except IOError as e:
+        print(os.strerror(e))
+        sys.exit(1)
