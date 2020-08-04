fb7c9ffd93050b2bd365d77cfef260d7232db6d8
Stephan Garland <stephan.garland@logicmonitor.com>
2020-08-04 14:41:02 -0500

    Improved Terragrunt handling

--- tf_12_upgrader.sh
+++ tf_12_upgrader.sh
@@ -9,17 +9,14 @@ fi
 printf "WARNING: This will recursively upgrade everything in \n$(pwd) with no confirmations.\n\n"
 read -p  "Continue? Only 'yes' will be accepted to confirm: "
 if [[ $REPLY == [Yy]es ]]; then
-    ls $(pwd)/*/ >/dev/null 2>&1
-    if [[ $? == 0 ]]; then
-        find . -type f -name "main.tf" \
-        -execdir terraform init \; -execdir terraform 0.12upgrade -yes \; -execdir touch terraform_0.12_ready \;
-    else
-        if terraform init && terraform 0.12upgrade -yes; then
-            touch terraform_0.12_ready
-        else
-            echo "Couldn't upgrade $i to Terraform 0.12"
+    find . -type f -name "Makefile" \
+        -execdir pwd \; -execdir make init \; -execdir terraform 0.12upgrade -yes \; -execdir touch terraform_0.12_ready \;
+    readarray NO_MAKEFILE < <(find . -type f -not -name "terraform_0.12_ready" -exec dirname {} \; | grep -Ev ".terraform|.terragrunt-cache" | sort -u)
+    for i in ${FILES[@]}; do
+        if [[ -e main.tf ]]; then
+            pwd && terraform init $i && terraform 0.12upgrade -yes
         fi
-    fi
+    done
 else
     echo "Exiting"
     exit
