a5dd3f04ca5a3fa01486cb5aaad96d109b230992
Stephan Garland <stephan.garland@logicmonitor.com>
2020-08-04 12:01:07 -0500

    changed to usr/bin/env, changed for loop to execdir
    Signed-off-by: Stephan Garland <stephan.garland@logicmonitor.com>

--- tf_12_ready_cleanup.sh
+++ tf_12_ready_cleanup.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 read -p  "Do you want to delete all tf_0.12_ready files? Only 'yes' will be accepted to confirm: "
 if [[ $REPLY == [Yy]es ]]; then
--- tf_12_upgrader.sh
+++ tf_12_upgrader.sh
@@ -1,28 +1,26 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
 if [[ ! $(terraform version | grep v0.12) ]]; then
     echo "The 0.12upgrade tool is only available in Terraform >= 0.12.x"
     echo "Please switch to Terraform 0.12.x"
     exit 1
 fi
+
 printf "WARNING: This will recursively upgrade everything in \n$(pwd) with no confirmations.\n\n"
 read -p  "Continue? Only 'yes' will be accepted to confirm: "
 if [[ $REPLY == [Yy]es ]]; then
     ls $(pwd)/*/ >/dev/null 2>&1
     if [[ $? == 0 ]]; then
-        for i in $(ls); do
-            echo $i && terraform init $i &&  terraform 0.12upgrade -yes $i && touch $i/terraform_0.12_ready
-        done
-        for i in $(ls); do
-            if [[ ! -f $i/terraform_0.12_ready ]]; then
-                echo "Couldn't upgrade $i to Terraform 0.12"
-            fi
-        done
+        find . -type f -name "main.tf" \
+        -execdir terraform init \; -execdir terraform 0.12upgrade -yes \; -execdir touch terraform_0.12_ready \;
     else
-        terraform init && terraform 0.12upgrade -yes && touch terraform_0.12_ready
-        if [[ ! -f terraform_0.12_ready ]]; then
-            printf "\nCouldn't upgrade to Terraform 0.12\n"
+        if terraform init && terraform 0.12upgrade -yes; then
+            touch terraform_0.12_ready
+        else
+            echo "Couldn't upgrade $i to Terraform 0.12"
         fi
     fi
 else
     echo "Exiting"
+    exit
 fi
