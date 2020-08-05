dec135f3ce0fdf174191b296594131f27f3c7b5d
Stephan Garland <stephan.garland@logicmonitor.com>
2020-08-05 14:43:31 -0500

    forgot shopt

--- tf_12_check_missing.sh
+++ tf_12_check_missing.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-
+shopt -s globstar
 printf "Checking in $(pwd) and all subdirectories for missing terraform_0.12_ready\n\n"
 for i in **/; do
     if [[ ! -e $i/terraform_0.12_ready ]] && [[ $(ls $i*.tf | wc -l) -ge 1 ]] 2>/dev/null; then 
